338a560b3501b1457b314e6dbdf0219c104d5c5b
theZalmanian <thezalmanian@gmail.com>
2023-10-19 10:17:17 -0700

    Setup order-summary.php

---
+++ practice/pizza-ordering/order-summary.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+    <?php echo '<p>POST</p>'; ?>
+    <?php echo var_dump($_POST); ?>
+
+    <?php echo '<p>GET</p>'; ?>
+    <?php echo var_dump($_GET); ?>
+    </body>
+</html>
