6d124a6b6756488c1bbb1bb860103c58a3a204dd
LeviAdamMiller <LAM614@outlook.com>
2023-10-25 15:46:09 -0700

    fixed current page on contact page

---
+++ contact.php
@@ -0,0 +1,26 @@
+
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$message = $_POST["message"];
+
+$sendToAddress = "levi.spam3@outlook.com";
+$sendFromAddress = "NursingNucleus@greenriver.com";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: $sendFromAddress" . "\r\n";
+
+$subject = "Nursing Nucleus Contact Page";
+
+$contactPage = "<ul>
+                  <il>Name: $name </il><br>
+                  <il>Email: $email </il><br>
+                  <il>Phone: $phone </il><br>
+                  <il>Message:$message</il>
+                   </ul>";
+
+
+mail($sendToAddress,$subject,$contactPage,$headers);
+
