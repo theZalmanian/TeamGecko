bfbdbbc3aad0ebea09b1183a33182206dd6f5848
theZalmanian <thezalmanian@gmail.com>
2023-10-25 20:20:28 -0700

    Connected Experience form to confirm.php on submit - If user accesses confirm.php w/o doing survey, links then there

--- php/confirm.php
+++ php/confirm.php
@@ -17,7 +17,27 @@
         </div>
         <div class="col-12 col-md-8 col-lg-4">
             <div class="card text-center mt-3">
-                <p>Hello World</p>
+                <?php
+                    // check that all required questions were answered on the experience survey
+                    if( isset($_POST["q1-site-attended"]) &&
+                        isset($_POST["q2-enjoyed-site"]) &&
+                        isset($_POST["q3-staff-supportive"]) &&
+                        isset($_POST["q4-site-learning-objectives"]) &&
+                        isset($_POST["q5-preceptor-learning-objectives"]) &&
+                        isset($_POST["q6-recommend-site"]) ) {
+                        echo var_dump($_POST);
+                    }
+                    else {
+                        echo "<div class='h4 p-3 mb-0'>
+                                <p>
+                                    Please fill out the Clinical Experience Questionnaire:
+                                </p>
+                                <a class='nav-link btn' href='/sprint-2/experience.html'>
+                                    Experience Form
+                                </a>
+                              </div>";
+                    }
+                ?>
             </div>
         </div>
         <div class="col-md-2 col-lg-4">
