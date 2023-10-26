8751f3b77e51aafc68161c0109acaf40f2968835
theZalmanian <thezalmanian@gmail.com>
2023-10-25 20:33:45 -0700

    Added green confirm button linking to receipt.php - Added red cancel button leading back to form

--- php/confirm.php
+++ php/confirm.php
@@ -13,19 +13,23 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-md-2 col-lg-4">
+        <div class="col-md-2">
         </div>
-        <div class="col-12 col-md-8 col-lg-4">
+        <div class="col-12 col-md-8">
             <div class="card text-center mt-3">
                 <?php
                     // check that all required questions were answered on the experience survey
-                    if( isset($_POST["q1-site-attended"]) &&
-                        isset($_POST["q2-enjoyed-site"]) &&
-                        isset($_POST["q3-staff-supportive"]) &&
-                        isset($_POST["q4-site-learning-objectives"]) &&
-                        isset($_POST["q5-preceptor-learning-objectives"]) &&
-                        isset($_POST["q6-recommend-site"]) ) {
-                        echo var_dump($_POST);
+                    if( isset($_POST["q1-site-attended"]) && isset($_POST["q2-enjoyed-site"])
+                        && isset($_POST["q3-staff-supportive"]) && isset($_POST["q4-site-learning-objectives"])
+                        && isset($_POST["q5-preceptor-learning-objectives"]) && isset($_POST["q6-recommend-site"]) ) {
+                ?>
+
+                <form action="/php/receipt.php" method="post">
+                    <a class="btn btn-danger" href="/sprint-2/experience.html">Cancel</a>
+                    <button class="btn btn-success py-2 my-2" id="submit-experience">Confirm</button>
+                </form>
+
+                <?php
                     }
                     else {
                         echo "<div class='h4 p-3 mb-0'>
@@ -40,7 +44,7 @@
                 ?>
             </div>
         </div>
-        <div class="col-md-2 col-lg-4">
+        <div class="col-md-2">
         </div>
     </div>
 </div>
