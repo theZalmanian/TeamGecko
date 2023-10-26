5245f6347f82f33e87ff471436619f1c6d597c4b
theZalmanian <thezalmanian@gmail.com>
2023-10-25 17:43:02 -0700

    Updated vardump output to be more readable

--- vardump.php
+++ vardump.php
@@ -1,12 +1,16 @@
-<html>
+<html lang="en">
     <head>
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>POST</p>'; ?>
-        <?php echo var_dump($_POST); ?>
+        <?php echo "<p>POST</p>"; ?>
+        <pre>
+            <?php echo var_dump($_POST); ?>
+        </pre>
 
-        <?php echo '<p>GET</p>'; ?>
-        <?php echo var_dump($_GET); ?>
+        <?php echo "<p>GET</p>"; ?>
+        <pre>
+            <?php echo var_dump($_GET); ?>
+        </pre>
     </body>
 </html>
