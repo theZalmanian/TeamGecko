6a165d98fac23082fd9fd07158e30d52e702ea32
LeviAdamMiller <LAM614@outlook.com>
2023-10-30 15:18:43 -0700

    Changed to NursingNucleus@greenriverdev.com Accounting for if $phone is empty echos your message was received

--- php/contact.php
+++ php/contact.php
@@ -6,7 +6,7 @@ $phone = $_POST["phone"];
 $message = $_POST["message"];
 
 $sendToAddress = "levi.spam3@outlook.com";
-$sendFromAddress = "NursingNucleus@greenriver.com";
+$sendFromAddress = "NursingNucleus@greenriverdev.com";
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
@@ -21,6 +21,22 @@ $contactPage = "<ul>
                   <il>Message:$message</il>
                    </ul>";
 
+if ($phone == ""){
+
+    $contactPage = "<ul>
+                  <il>Name: $name </il><br>
+                  <il>Email: $email </il><br>
+                  <il>Message:$message</il>
+                   </ul>";
+} else {
+    $contactPage = "<ul>
+                  <il>Name: $name </il><br>
+                  <il>Email: $email </il><br>
+                  <il>Phone: $phone </il><br>
+                  <il>Message:$message</il>
+                   </ul>";
+}
 
 mail($sendToAddress,$subject,$contactPage,$headers);
 
+echo" Your message was received";
