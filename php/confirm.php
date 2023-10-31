b24a678f801af0184f79b10cf99dde3526ddc988
theZalmanian <thezalmanian@gmail.com>
2023-10-30 19:56:57 -0700

    Created individual css file for each sprint to avoid breaking any previous styling

--- php/confirm.php
+++ php/confirm.php
@@ -162,15 +162,15 @@
     function displayQuestion($questionNum, $questionText, $response) {
         // setup display card w/ the given values
         $displayCard = "<div class='card p-3 my-1'>
-                        <ul class='list-group list-group-flush'>
-                        <li class='list-group-item'>
-                            {$questionNum}. {$questionText}
-                        </li>
-                        <li class='list-group-item'>
-                            {$response} 
-                        </li>
-                        </ul>
-                    </div>";
+                            <ul class='list-group list-group-flush'>
+                                <li class='list-group-item'>
+                                    {$questionNum}. {$questionText}
+                                </li>
+                                <li class='list-group-item'>
+                                    {$response} 
+                                </li>
+                            </ul>
+                        </div>";
 
         return $displayCard;
     }
