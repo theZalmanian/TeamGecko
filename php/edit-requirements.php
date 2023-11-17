04979c7ae161f1ae40ef9b7d71b58e0cbf13ace1
theZalmanian <thezalmanian@gmail.com>
2023-11-17 02:44:53 -0800

    Getting and displaying all clinical requirements currently in the DB on edit-requirements.php

--- php/edit-requirements.php
+++ php/edit-requirements.php
@@ -9,14 +9,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <?php 
+    <?php 
         // include standard nursing header metadata
-        require "../php/layouts/nursing-metadata.php";
+        require_once("../php/layouts/nursing-metadata.php");
     ?>
 </head>
 <body>
 	<main class="container mt-3">
 		<div class="row">
+            <?php 
+                // setup and execute SELECT Query
+                $allRequirements = executeQuery("SELECT * FROM ClinicalRequirements");
+
+                $targetCount = 0;
+                // run through rows returned from query
+                while ($currRow = mysqli_fetch_assoc($allRequirements)) {
+                    // get each column from current row
+                    $title = $currRow["RequirementTitle"];
+                    $notes = $currRow["RequirementNotes"];
+                    $option1 = $currRow["Option1"];
+                    $option2 = $currRow["Option2"];
+
+                    // generate an accordion item for the row
+                    echo $title . "<br><br>" . $notes . "<br><br>" . $option1 . "<br><br>" . $option2;
+                    echo "<br><br>";
+                }
+            ?>
         </div>
     </main>
 </body>
