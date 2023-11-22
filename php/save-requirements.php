da6db4d4fe99904986d2b1fdc972d06c47694010
theZalmanian <thezalmanian@gmail.com>
2023-11-22 13:05:33 -0800

    Removed debugging and test code from save-requirements - Only getting a requirement from DB once instead of for every column - Modified page layout - Displaying success message with link to requirements page

--- php/save-requirements.php
+++ php/save-requirements.php
@@ -4,6 +4,10 @@
 
     // store the current page's title for dynamic HTML generation
     $currPageTitle = "Save Requirements";
+
+	$lastRequirementID = -1;
+
+    $requirementInDB = array();
 ?>
 
 <!DOCTYPE html>
@@ -17,54 +21,50 @@
 <body>
 	<main class="container mt-3">
 		<div class="row">
-           <div>
-                <pre>
-                    <?php 
-                        // echo var_dump($_POST) 
-                    ?>
-                </pre>
-
+            <div class="col-md-2 col-lg-4">
+            </div>
+            <div class="col-12 col-md-8 col-lg-4">
                 <?php
+                    $updateCount = "";
                     // if page was accessed from edit-requirements.php
                     if(isset($_POST["confirm-edits"]) && $_POST["confirm-edits"] === "confirmed") {
-                        $debugCount = 0;
                         // loop through the post data
                         foreach ($_POST as $currKey => $currValue) {
                             if($currKey != "confirm-edits") {
-                                $debugCount++;
                                 // separate the key into ID and column
                                 $splitKey = explode("-", $currKey);
-                                $currRequirementID = $data[0];
-                                $currColumnName = $data[1];
+                                $currRequirementID = $splitKey[0];
+                                $currColumnName = $splitKey[1];
 
-                                // get the version of current requirement stored in DB
-                                $selectResult = executeQuery("SELECT * 
-                                                              FROM ClinicalRequirements 
-                                                              WHERE RequirementID = '{$currRequirementID}'");
+                                if($currRequirementID != $lastRequirementID) {
+                                    // get the version of current requirement stored in DB
+                                    $selectResult = executeQuery("SELECT *
+                                                                  FROM ClinicalRequirements 
+                                                                  WHERE RequirementID = '{$currRequirementID}'");
 
-                                $requirementInDB = mysqli_fetch_assoc($selectResult);
+                                    $requirementInDB = mysqli_fetch_assoc($selectResult);
+
+                                    $lastRequirementID = $currRequirementID;
+                                }
 
                                 // only update if the current value is not the same as that stored in DB
                                 if($requirementInDB[$currColumnName] != $currValue) {
                                     // update the column with the given value
-                                    $result = executeQuery("UPDATE ClinicalRequirements 
-                                                            SET {$currColumnName} = '{$currValue}' 
-                                                            WHERE RequirementID = '{$currRequirementID}'");
-
-                                    // display result
-                                    if ($result) {
-                                        echo "<p>({$debugCount}) Update successful!</p>";
-                                    } 
-                                    
-                                    else {
-                                        echo "<p>Update failed: " . mysqli_error($dbConnection) . "</p>";
-                                    }
+                                    executeQuery("UPDATE ClinicalRequirements 
+                                                  SET {$currColumnName} = '{$currValue}' 
+                                                  WHERE RequirementID = '{$currRequirementID}'");
                                 }
                             }
                         }
+                        
+                        echo displayMessageWithLink("/sprint-4/requirements.php", "Clinical Requirements",
+                                                    "All changes were saved successfully");
+                    
                     }
                 ?>
-           </div>
+            </div>
+            <div class="col-md-2 col-lg-4">
+            </div>
         </div>
     </main>
 </body>
