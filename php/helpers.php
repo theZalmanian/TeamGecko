75ad7ee66384759d914fb498e6180320d07df070
theZalmanian <thezalmanian@gmail.com>
2023-11-22 13:48:16 -0800

    Updated structure of display helpers so title is optional and message is required - Updated pages that use those methods to use them accordingly

--- php/helpers.php
+++ php/helpers.php
@@ -8,17 +8,25 @@
      * along with the given link styled as a button at the bottom
      * @param string $linkHref The links destination path
      * @param string $linkText The text to be displayed in the "button" link
-     * @param string $title The title of the message block
-     * @param string $message (Optional) The message itself, may be unnecessary if only a title is needed
+     * @param string $message The message itself
+     * @param string $title (Optional) The title of the message block, may be unnecessary if only a message is needed
      * @return string a Bootstrap Alert containing the given HTML content
      */
-    function displayMessageWithLink($linkHref, $linkText, $title, $message = "") {
+    function displayMessageWithLink($linkHref, $linkText, $message, $title = "") {
         // setup message content
-        $messageContent = "<h2>{$title}</h2>
-                           <hr>
-                           <h4>{$message}</h4>
-                           <hr>
-                           <a class='btn btn-success py-2 m-2 border' href='{$linkHref}'>{$linkText}</a>";
+        $messageContent = "";
+        
+        // if a title was given
+        if(!empty($title)) {
+            // display it
+            $messageContent .= "<h2>{$title}</h2>
+                                <hr>";
+        }
+
+        // add message, and setup link using given attributes
+        $messageContent .= "<h4>{$message}</h4>
+                            <hr>
+                            <a class='btn btn-success py-2 m-2 border' href='{$linkHref}'>{$linkText}</a>";
 
         // place and return message content inside Bootstrap Alert
         return displayBootstrapAlert($messageContent);
@@ -26,15 +34,23 @@
 
     /**
      * Returns a Bootstrap Alert to be displayed containing the given title and message
-     * @param string $title The title of the message block
-     * @param string $message (Optional) The message itself, may be unnecessary if only a title is needed
+     * @param string $message The message itself
+     * @param string $title (Optional) The title of the message block, may be unnecessary if only a message is needed
      * @return string a Bootstrap Alert containing the given HTML content
      */
-    function displayMessage($title, $message = "") {
+    function displayMessage($message, $title = "") {
         // setup message content
-        $messageContent = "<h2>{$title}</h2>
-                           <hr>
-                           <h4>{$message}</h4>";
+        $messageContent = "";
+
+        // if a title was given
+        if(!empty($title)) {
+            // display it
+            $messageContent .= "<h2>{$title}</h2>
+                                <hr>";
+        }
+
+        // add message to display
+        $messageContent .= "<h4>{$message}</h4>";
 
         // place and return message content inside Bootstrap Alert
         return displayBootstrapAlert($messageContent);
@@ -99,6 +115,33 @@
 
         // execute query and capture result
         $result = mysqli_query($dbConnection, $query);
+
+        // close connection to DB
+        disconnectDB($dbConnection);
+
+        return $result;
+    }
+
+    /**
+     * Opens a connection to the DB, executes the given query, displays success or error message,
+     * closes the DB connection, and returns the result
+     *
+     * @param string $query The SQL query to be executed
+     * @return mixed Returns a mysqli_result object for successful SELECT queries, and true/false for CREATE, UPDATE, and DELETE queries
+     */
+    function executeQueryDebugging($query) {
+        // open connection to DB
+        $dbConnection = connectDB();
+
+        // execute query and capture result
+        $result = mysqli_query($dbConnection, $query);
+
+        // display success or fail
+        if ($result) {
+            echo "<p>Query successful!</p>";
+        } else {
+            echo "<p>Query failed" . mysqli_error($dbConnection)  . "</p>";
+        }
 
         // close connection to DB
         disconnectDB($dbConnection);
