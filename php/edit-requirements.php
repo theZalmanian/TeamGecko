8353b0c015d625431b32fff53c25f047d27169bd
theZalmanian <thezalmanian@gmail.com>
2023-11-22 21:28:46 -0800

    (#31) Setup basic Scrollspy for edit-requirements

--- php/edit-requirements.php
+++ php/edit-requirements.php
@@ -15,41 +15,74 @@
     ?>
 </head>
 <body>
-	<main>
-        <form class="container mt-3" action="/php/save-requirements.php" method="post" target="_blank">
-            <input type="hidden" value="confirmed" name="confirm-edits">
-            <div class="row d-flex justify-content-center">
-                <?php 
-                    // setup and execute SELECT Query
-                    $allRequirements = executeQuery("SELECT * 
-                                                    FROM ClinicalRequirements");
-
-                    // run through rows returned from query
-                    while ($currRow = mysqli_fetch_assoc($allRequirements)) {
-                        // get each column from current row
-                        $requirementID = $currRow["RequirementID"];
-                        $title = $currRow["RequirementTitle"];
-                        $notes = $currRow["RequirementNotes"];
-                        $option1 = $currRow["Option1"];
-                        $option2 = $currRow["Option2"];
-
-                        // display the current requirement in editable inputs
-                        echo displayRequirement($title, $notes, $option1, $option2, $requirementID);
-                    }                
-                ?>
-                <div class="col-12 col-md-6 card rounded-0 p-3 sticky-bottom">
-                    <div class="row d-flex justify-content-center">
-                        <button class="col-5 btn btn-success py-2 mx-2 border" id="save-requirements">Save All Changes</button>
-                        <a class="col-5 btn btn-danger py-2 mx-2 border" href="/sprint-4/requirements.php">Cancel</a>
-                    </div>
+	<main class="container mt-3">
+        <div class="row">
+            <div class="col-md-2 col-lg-3">
+                <div class="card col-12 h-100 rounded-0">
+                    <nav class="navbar p-3 sticky-top" id="scroll-spy">
+                        <div class="navbar-nav">
+                            <a class="nav-link" href="#spy-1">
+                                How it works
+                            </a>
+                            <a class="nav-link" href="#spy-2">
+                                How it works
+                            </a>
+                            <a class="nav-link" href="#spy-3">
+                                How it works
+                            </a>
+                            <a class="nav-link" href="#spy-4">
+                                How it works
+                            </a>
+                            <a class="nav-link" href="#spy-5">
+                                How it works
+                            </a>
+                            <a class="nav-link" href="#spy-6">
+                                How it works
+                            </a>
+                        </div>
+                    </nav>     
                 </div>
             </div>
-        </form>
+            <div class="col-12 col-md-10 col-lg-9">
+                <form class="container" data-bs-spy='scroll' data-bs-target='#scroll-spy' 
+                    action="/php/save-requirements.php" method="post" target="_blank">
+                    <input type="hidden" value="confirmed" name="confirm-edits">
+                    <div class="row justify-content-center">
+                        <?php 
+                            // setup and execute SELECT Query
+                            $allRequirements = executeQuery("SELECT * 
+                                                            FROM ClinicalRequirements");
+        
+                            // run through rows returned from query
+                            while ($currRow = mysqli_fetch_assoc($allRequirements)) {
+                                // get each column from current row
+                                $requirementID = $currRow["RequirementID"];
+                                $title = $currRow["RequirementTitle"];
+                                $notes = $currRow["RequirementNotes"];
+                                $option1 = $currRow["Option1"];
+                                $option2 = $currRow["Option2"];
+        
+                                // display the current requirement in editable inputs
+                                echo displayRequirement($title, $notes, $option1, $option2, $requirementID);
+                            }                
+                        ?>
+                        <div class="card col-12 col-md-10 p-3 sticky-bottom  rounded-0">
+                            <div class="row d-flex justify-content-center">
+                                <button class="col-5 btn btn-success py-2 mx-2 border" id="save-requirements">Save All Changes</button>
+                                <a class="col-5 btn btn-danger py-2 mx-2 border" href="/sprint-4/requirements.php">Cancel</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </main>
 </body>
 </html>
 
 <?php
+    $spyCounter = 0;
+
     /**
      * 
      * @param string $title
@@ -59,7 +92,11 @@
      * @return string 
      */
     function displayRequirement($title, $notes, $option1, $option2, $targetID) {
-        return "<div class='card col-12 col-md-6 mx-md-1 mb-3 p-3'>
+        global $spyCounter;
+
+        $spyCounter++;
+
+        return "<div class='card col-12 col-md-10 mx-md-1 mb-3 p-3' id='spy-{$spyCounter}'>
                     " . generateBootstrapFloatingLabelTextbox("{$targetID}-RequirementTitle", "Title", $title, true) . "
                     " . generateBootstrapFloatingLabelTextbox("{$targetID}-RequirementNotes", "Notes", $notes, false) . "
                     " . displayOptionTextarea("{$targetID}-Option1", "Option 1", $option1, true) . "
@@ -118,5 +155,4 @@
                 </div>";
         }
     }
-
 ?>
