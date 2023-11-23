029fcde44d55dc8f101a6ac9f8ed517fbad9f796
theZalmanian <thezalmanian@gmail.com>
2023-11-22 23:26:02 -0800

    (#31) Made scrollspy dynamic - Modified layout and styling of edit-requirements elements

--- php/edit-requirements.php
+++ php/edit-requirements.php
@@ -4,6 +4,46 @@
 
     // store the current page's title for dynamic HTML generation
     $currPageTitle = "Edit Requirements";
+
+    $allRequirementTitles = array();
+    $allRequirementsDisplay = array();
+?>
+
+<?php 
+    // setup and execute SELECT Query
+    $allRequirements = executeQuery("SELECT * 
+                                    FROM ClinicalRequirements");
+
+    // run through rows returned from query
+    while ($currRow = mysqli_fetch_assoc($allRequirements)) {
+        // get each column from current row
+        $requirementID = $currRow["RequirementID"];
+        $title = $currRow["RequirementTitle"];
+        $notes = $currRow["RequirementNotes"];
+        $option1 = $currRow["Option1"];
+        $option2 = $currRow["Option2"];
+
+        $allRequirementTitles[] = $title;
+
+        // display the current requirement in editable inputs
+        $allRequirementsDisplay[] = displayRequirement($title, $notes, $option1, $option2, $requirementID);
+    }     
+    
+    function displayScrollspy() {
+        global $allRequirementTitles;
+
+        $scrollSpy = "<div class='card col-12 h-100 border-bottom-0'>
+                        <nav class='navbar p-3 rounded-3 sticky-top' id='scrollspy'>
+                            <div class='navbar-nav'>";
+
+        for ($i = 1; $i <= count($allRequirementTitles); $i++) {
+            $scrollSpy .= "<a class='nav-link ps-2 py-0 my-2' href='#spy-{$i}'>
+                               {$allRequirementTitles[$i - 1]}
+                            </a>";
+        }
+
+        return $scrollSpy . "</div></nav></div>";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -18,55 +58,21 @@
 	<main class="container mt-3">
         <div class="row">
             <div class="col-md-2 col-lg-3">
-                <div class="card col-12 h-100 rounded-0">
-                    <nav class="navbar p-3 sticky-top" id="scroll-spy">
-                        <div class="navbar-nav">
-                            <a class="nav-link" href="#spy-1">
-                                How it works
-                            </a>
-                            <a class="nav-link" href="#spy-2">
-                                How it works
-                            </a>
-                            <a class="nav-link" href="#spy-3">
-                                How it works
-                            </a>
-                            <a class="nav-link" href="#spy-4">
-                                How it works
-                            </a>
-                            <a class="nav-link" href="#spy-5">
-                                How it works
-                            </a>
-                            <a class="nav-link" href="#spy-6">
-                                How it works
-                            </a>
-                        </div>
-                    </nav>     
-                </div>
+                <?php 
+                    echo displayScrollspy();
+                ?>
             </div>
             <div class="col-12 col-md-10 col-lg-9">
-                <form class="container" data-bs-spy='scroll' data-bs-target='#scroll-spy' 
+                <form class="container" data-bs-spy='scroll' data-bs-target='#scrollspy' 
                     action="/php/save-requirements.php" method="post" target="_blank">
                     <input type="hidden" value="confirmed" name="confirm-edits">
                     <div class="row justify-content-center">
-                        <?php 
-                            // setup and execute SELECT Query
-                            $allRequirements = executeQuery("SELECT * 
-                                                            FROM ClinicalRequirements");
-        
-                            // run through rows returned from query
-                            while ($currRow = mysqli_fetch_assoc($allRequirements)) {
-                                // get each column from current row
-                                $requirementID = $currRow["RequirementID"];
-                                $title = $currRow["RequirementTitle"];
-                                $notes = $currRow["RequirementNotes"];
-                                $option1 = $currRow["Option1"];
-                                $option2 = $currRow["Option2"];
-        
-                                // display the current requirement in editable inputs
-                                echo displayRequirement($title, $notes, $option1, $option2, $requirementID);
-                            }                
+                        <?php
+                            foreach ($allRequirementsDisplay as $requirementDisplay) {
+                                echo $requirementDisplay;
+                            }
                         ?>
-                        <div class="card col-12 col-md-10 p-3 sticky-bottom  rounded-0">
+                        <div class="card col-12 col-md-10 p-3 border-bottom-0 rounded-0 sticky-bottom">
                             <div class="row d-flex justify-content-center">
                                 <button class="col-5 btn btn-success py-2 mx-2 border" id="save-requirements">Save All Changes</button>
                                 <a class="col-5 btn btn-danger py-2 mx-2 border" href="/sprint-4/requirements.php">Cancel</a>
