59d79d6bdeb0dec74e017f8caa3894e4862209b5
theZalmanian <thezalmanian@gmail.com>
2023-11-23 17:03:26 -0800

    (#31) Made scrollspy collapse on mobile through JS - Fixed bug with generate textarea inputs

--- php/edit-requirements.php
+++ php/edit-requirements.php
@@ -32,9 +32,9 @@
     function displayScrollspy() {
         global $allRequirementTitles;
 
-        $scrollSpy = "<div class='card col-12 h-100 border-bottom-0'>
-                        <nav class='navbar p-3 rounded-3 sticky-top' id='scrollspy'>
-                            <div class='navbar-nav'>";
+        $scrollSpy = "<div class='card col-12 mb-3 mb-md-0 h-100 border-bottom-0 rounded-bottom-0' id='scrollspy-container'>
+                            <nav class='navbar p-3 rounded-3 sticky-md-top' id='scrollspy'>
+                                <div class='navbar-nav'>";
 
         for ($i = 1; $i <= count($allRequirementTitles); $i++) {
             $scrollSpy .= "<a class='nav-link ps-2 py-0 my-2' href='#spy-{$i}'>
@@ -57,14 +57,20 @@
 <body>
 	<main class="container mt-3">
         <div class="row">
-            <div class="col-md-2 col-lg-3">
+            <div class="col-md-3 col-lg-3">
+                <button id="scrollspy-toggler" class="btn btn-success d-md-none w-100 py-2 mb-2 border">
+                    Go to Clinical Site
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-expand" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z"/>
+                    </svg>
+                </button>
                 <?php 
                     echo displayScrollspy();
                 ?>
             </div>
-            <div class="col-12 col-md-10 col-lg-9">
-                <form class="container" data-bs-spy='scroll' data-bs-target='#scrollspy' 
-                    action="/php/save-requirements.php" method="post" target="_blank">
+            <div class="col-12 col-md-9 col-lg-9">
+                <form class="container" action="/php/save-requirements.php" method="post"
+                    data-bs-spy='scroll' data-bs-target='#scrollspy' data-bs-smooth-scroll='true'>
                     <input type="hidden" value="confirmed" name="confirm-edits">
                     <div class="row justify-content-center">
                         <?php
@@ -74,7 +80,7 @@
                         ?>
                         <div class="card col-12 col-md-10 p-3 border-bottom-0 rounded-0 sticky-bottom">
                             <div class="row d-flex justify-content-center">
-                                <button class="col-5 btn btn-success py-2 mx-2 border" id="save-requirements">Save All Changes</button>
+                                <button type="submit" class="col-5 btn btn-success py-2 mx-2 border" id="save-requirements">Save All Changes</button>
                                 <a class="col-5 btn btn-danger py-2 mx-2 border" href="/sprint-4/requirements.php">Cancel</a>
                             </div>
                         </div>
@@ -83,6 +89,82 @@
             </div>
         </div>
     </main>
+
+    <script>
+        "use strict"; // make JS be more like Java
+
+        // when the page loads
+        window.addEventListener('load', function () {
+            // setup to respond to page resizing 
+            window.addEventListener('resize', modifyScrollspy);
+
+            setupButtonOnClick("scrollspy-toggler", toggleScrollspy);
+
+            // setup when page first loads, in case user is on mobile
+            modifyScrollspy();
+        })
+
+        /**
+         * The standard mobile breakpoint is 576px
+         */ 
+        const MOBILE_BREAKPOINT = 765;
+
+        /**
+         * Returns true if the current window is <= 576px; otherwise false
+         */ 
+        function screenIsMobile() {
+            return window.innerWidth < MOBILE_BREAKPOINT;
+        }
+
+        function toggleScrollspy() {
+            const scrollspyContainer = getByID("scrollspy-container");
+
+            if(scrollspyContainer.classList.contains("collapse")) {
+                scrollspyContainer.classList.remove("collapse");
+            }
+            else {
+                scrollspyContainer.classList.add("collapse");
+            }
+        }
+
+        function modifyScrollspy() {   
+            const scrollspyContainer = getByID("scrollspy-container");
+
+            // if the screen is currently mobile-sized
+            if (screenIsMobile()) {
+                scrollspyContainer.classList.remove("h-100", "border-bottom-0", "rounded-bottom-0");
+                scrollspyContainer.classList.add("collapse");
+            } 
+            
+            // if the screen size changed, but is not small enough to be considered mobile 
+            else {
+                scrollspyContainer.classList.add("h-100", "border-bottom-0", "rounded-bottom-0");
+                scrollspyContainer.classList.remove("collapse");
+            }
+        }
+
+        /**
+         * Shortened form of the document.getElementById method
+         * @param elementID The element's id
+         * @returns The corresponding HTML Element
+         */
+        function getByID(elementID) {
+            return document.getElementById(elementID);
+        }
+
+        /**
+         * Sets up an onclick event for the given button using the given function
+         * @param {number} buttonID The button's id
+         * @param useFunction The function to be called when button is clicked
+         */
+        function setupButtonOnClick(buttonID, useFunction) {
+            // get the button
+            const button = getByID(buttonID);
+
+            // set it's onclick event
+            button.addEventListener("click", useFunction);
+        }
+    </script>
 </body>
 </html>
 
@@ -145,7 +227,7 @@
     function displayOptionTextarea($inputID, $inputLabelText, $value, $isRequired) {
         if($isRequired) {
             return "<div class='contact form-floating my-2'>
-                        <textarea class='form-control' id='{$inputID}' name='{$inputID}' required>{$value}</textarea>
+                        <textarea class='form-control' id='{$inputID}' name='{$inputID}' placeholder='' required>{$value}</textarea>
                         <label for='{$inputID}'>
                             {$inputLabelText}" . displayRequired() . "
                         </label>
@@ -154,7 +236,7 @@
 
         else {
             return "<div class='contact form-floating my-2'>
-                    <textarea class='form-control' id='{$inputID}' name='{$inputID}'>{$value}</textarea>
+                    <textarea class='form-control' id='{$inputID}' name='{$inputID}' placeholder=''>{$value}</textarea>
                     <label for='{$inputID}'>
                         {$inputLabelText}
                     </label>
