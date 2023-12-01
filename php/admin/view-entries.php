a2864f5b3df0a30b7451eaeb82a37ae0d800f7d7
theZalmanian <thezalmanian@gmail.com>
2023-12-01 12:13:35 -0800

    (#46) Added download icon to spreadsheet button

--- php/admin/view-entries.php
+++ php/admin/view-entries.php
@@ -146,7 +146,11 @@
                 <?php 
 					// setup "download .csv" button
 					$exportButton = "<a class='col-12 btn btn-success py-2 mt-3 w-100 border' id='export-spreadsheet'>
-										Export to .csv
+										Download Spreadsheet
+										<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-download' viewBox='0 0 16 16'>
+											<path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5'/>
+											<path d='M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z'/>
+										</svg>
 									</a>";
 
                     // generate scrollspy to track and link clinical sites, including the
