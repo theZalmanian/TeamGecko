f2797c79fb5029cef69bac32a6a390237ae56b7b
theZalmanian <thezalmanian@gmail.com>
2023-12-01 13:36:54 -0800

    (#45) Added accordion toggle button to requirements page

--- sprint-5/requirements.php
+++ sprint-5/requirements.php
@@ -32,11 +32,18 @@
                 <div class="card my-2 notes">
                     <ul class="list-group list-group-flush text-center">
                         <li class="list-group-item text-break px-2">
-                            <strong>2660*All vaccination proof must include full name, date of birth, and date of vaccine, titer (blood draw), or test</strong>
+                            <strong>All vaccination proof must include full name, date of birth, and date of vaccine, titer (blood draw), or test</strong>
                         </li>
                     </ul>
                 </div>
-
+                <div class="card col-12 my-2 p-3">
+                    <button id="toggle-requirements" class="btn btn-success w-100 py-2 border">
+                        Expand all Requirements
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-expand" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z"/>
+                        </svg>
+                    </button>
+                </div>
                 <div class="accordion mb-2 my-2" id="requirements-accordion">
                     <?php
                         // setup and execute SELECT Query
@@ -73,6 +80,8 @@
             </div>
         </div>
     </main>
+
+    <script src="/js/toggle-accordion-items.js"></script>
 </body>
 </html>
 
