63de482f3d802f1c1f085652e08c4d27c3e72444
theZalmanian <thezalmanian@gmail.com>
2023-12-05 11:46:06 -0800

    (#47) Added edit site name modal to view-entries

--- php/admin/view-entries.php
+++ php/admin/view-entries.php
@@ -330,24 +330,24 @@
 
 		// generate and return feedback Modal
 		return "<div class='modal fade text-start' id='submission-{$totalSubmissionCount}-feedback-modal' tabindex='-1' aria-labelledby='feedback-modal-label-{$totalSubmissionCount}' aria-hidden='true'>
-							<div class='modal-dialog modal-dialog-centered modal-dialog-scrollable'>
-								<div class='modal-content'>
-									<div class='modal-header'>
-										<h1 class='modal-title fs-5' id='submission-{$totalSubmissionCount}-feedback-modal'>
-											" . displayStrong("Feedback") . "
-										</h1>
-										<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'>
-										</button>
-									</div>
-									<div class='modal-body'>
-										<h6>" . displayStrong("Site or Staff Feedback") . "</h6>
-										<p>{$siteOrStaffFeedback}</p>
-										<h6>" . displayStrong("Instructor Feedback") . "</h6>
-										<p>{$instructorFeedback}</p>
-									</div>
-								</div>
+					<div class='modal-dialog modal-dialog-centered modal-dialog-scrollable'>
+						<div class='modal-content'>
+							<div class='modal-header'>
+								<h1 class='modal-title fs-5' id='submission-{$totalSubmissionCount}-feedback-modal'>
+									" . displayStrong("Feedback") . "
+								</h1>
+								<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'>
+								</button>
 							</div>
-						</div>";
+							<div class='modal-body'>
+								<h6>" . displayStrong("Site or Staff Feedback") . "</h6>
+								<p>{$siteOrStaffFeedback}</p>
+								<h6>" . displayStrong("Instructor Feedback") . "</h6>
+								<p>{$instructorFeedback}</p>
+							</div>
+						</div>
+					</div>
+				</div>";
 	}
 
 	/**
@@ -379,12 +379,47 @@
 		// generate clinical site averages using given data
 		$averageRatings = generateRatingAverages($ratingTotals, $submissionCount);
 
+		$editButton = "<button type='button' class='btn btn-success border' data-bs-toggle='modal' 
+							data-bs-target='#submission-{$scrollspyElementsCount}-edit-modal'
+							id='submission-{$scrollspyElementsCount}-edit-label'>
+							<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+								<path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+							</svg>
+						</button>";
+
+		$editModal = "<div class='modal fade text-start' id='submission-{$scrollspyElementsCount}-edit-modal' 
+						tabindex='-1' aria-labelledby='submission-{$scrollspyElementsCount}-edit-label' aria-hidden='true'>
+						<div class='modal-dialog modal-dialog-centered modal-dialog-scrollable'>
+							<div class='modal-content'>
+								<div class='modal-header'>
+									<h1 class='modal-title fs-5' id='submission-{$scrollspyElementsCount}-feedback-modal'>
+										Edit Site Name
+									</h1>
+									<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'>
+									</button>
+								</div>
+								<div class='modal-body'>
+									<form action='/php/admin/save-requirements.php' method='post'>
+									<div class='input-group'>
+										<input type='text' class='form-control' value='{$clinicalSiteName}' aria-label='Clinical Site Name'>
+										<button type='submit'class='btn btn-success border'>
+											Submit
+										</button>
+									</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>";
+
 		// generate and return table using given data
 		return "<div class='card mb-3 p-3 table-responsive clinical-site-container' id='spy-{$scrollspyElementsCount}'>
 					<div class='submission-container'>
 						<h1 class='text-center mb-3'>
 							<strong>{$clinicalSiteName}</strong>
+							{$editButton}
 						</h1>
+						{$editModal}
 						<table class='table table-bordered table-striped-columns align-middle m-0'>
 							<thead>
 								<tr class='text-center'>
