bbcb19199fd37e441491ecae41a0ea18333fdd19
theZalmanian <thezalmanian@gmail.com>
2023-12-05 02:44:32 -0800

    (#37) Refactoring export-to-spreadsheet.js - Breaking down things into methods - Renaming methods/variables - Updating documentation - Simplifying logic

--- js/export-to-spreadsheet.js
+++ js/export-to-spreadsheet.js
@@ -2,18 +2,55 @@
 
 // when the page loads
 window.addEventListener("load", function () {  
-    let formattedRows = formatCSV(); 
-
-    // setup export link
-    setupButtonOnClick("export-spreadsheet", downloadCSV(formattedRows));
+    // setup export button
+    setupOnClick("export-spreadsheet", downloadCSV("export-spreadsheet"));
 })
 
-function formatCSV() { 
-    // setup empty array to store all spreadsheet rows
-    let spreadsheetRows = []; 
+/**
+ * 
+ */
+const NOT_APPLICABLE = "N/A";
 
-    // setup headers for spreadsheet rows
-    let headers = [
+/**
+* All rows being included in the spreadsheet
+*/
+let processedSubmissionRows = []; 
+
+/**
+ * Takes the submission data on view-entries.php, places it within a .csv file,
+ * and ties the generated file to the download attribute of the given button
+ * @param {string} exportButtonID the ID of the button used to download the generated .csv file
+ */
+function downloadCSV(exportButtonID) { 
+    // format the submission data from view-entries appropriately, 
+    // and then place it within a newly generated .csv file
+    const spreadsheetFile = new Blob([formatSubmissionsCSV()], { type: "text/csv" }); 
+  
+    // setup url to download newly created .csv file
+    const downloadURL = window.URL.createObjectURL(spreadsheetFile);
+
+    // get the export button using the given ID
+    const exportButton = getByID(exportButtonID);
+  
+    // set it's href to the download link 
+    exportButton.setAttribute("href", downloadURL);
+  
+    // specify that it will be downloading the .csv file in
+    // the href attribute, with the given name
+    exportButton.setAttribute("download", "experience-survey-submissions.csv"); 
+} 
+
+/**
+ * Formats all experience submissions contained within tables on the view-entries page 
+ * into a string that could be processed into a .csv (Comma-Separated Values) spreadsheet
+ *
+ * @returns {string} all experience submissions contained within tables on the view-entries page 
+ */
+function formatSubmissionsCSV() { 
+    /**
+     * The first row of the spreadsheet, containing all headers
+     */
+    const headerRow = [
         "Clinical Site",
         "Enjoyed Site",
         "Staff Supportive",
@@ -24,107 +61,92 @@ function formatCSV() {
         "Instructor Feedback"
     ];
 
-    // add headers as first spreadsheet row
-    // (csv format for headers is comma separated)
-    spreadsheetRows.push( headers.join(",") );
+    processedSubmissionRows.push(headerRow.join(","));
 
     // get and run through all clinical site containers on view-entries page
-    for (const currContainer of getByClass("clinical-site-container")) {
-        // get the submissions container from within container
-        const submissionContainer = currContainer.querySelector(".submission-container");
+    for (const currSiteContainer of document.querySelectorAll(".clinical-site-container")) {
+        // get the submissions container from within site container
+        const submissionContainer = currSiteContainer.querySelector(".submission-container");
 
         // get the current clinical site's name
         const clinicalSiteName = submissionContainer.querySelector("h1").innerText;
 
-        // grab the <tbody>'s containing submission and average data for the current clinical site
-        const submissionsTbody = submissionContainer.querySelector("table tbody");
-
-        // run through all the rows, and process them for spreadsheet formatting
-        const submissionRows = processTable(submissionsTbody, clinicalSiteName);
-        
-        // add them to the spreadsheet
-        spreadsheetRows.push(submissionRows);
+        processTable(submissionContainer.querySelector("table tbody"), clinicalSiteName);
     }
   
-    // return all spreadsheet rows as a string, each row on it's own line
-    return spreadsheetRows.join("\n"); 
+    // return all clinical site rows within a string, 
+    // each string of rows per clinical site separated by a newline
+    console.log(processedSubmissionRows);
+    return processedSubmissionRows.join("\n"); 
 } 
 
+/**
+ * 
+ * @param {*} currTbody 
+ * @param {*} clinicalSiteName 
+ * @returns 
+ */
 function processTable(currTbody, clinicalSiteName) {
-    // get all rows in current table
+    /**
+     * All \<tr\>'s in the given \<tbody\>
+     */
     const tableRows = currTbody.querySelectorAll("tr");
-    
-    // setup array to hold the rows formatted for spreadsheet
-    const formattedTableRows = [];
 
-    // run through all rows in current table
-    for (let i = 0; i < tableRows.length; i++) {
-        // get all rating (number) columns in the current row
-        const ratingColumns = tableRows[i].querySelectorAll("td.rating-column");
-
-        // setup array to hold the values in each column
-        const submissionColumns = [];
-
-        // add the clinical site name to each row as first column
-        submissionColumns.push(clinicalSiteName);
-
-        // run through all rating columns in row
-        for (let j = 0; j < ratingColumns.length; j++) {
-            // get the <td>'s class which stores the value of the rating column
-            // we are storing the value in the class, because they are displayed as ★'s on the page 
-            let columnValue = ratingColumns[j].classList.item(1);
-
-            // will come back as value-#, only add the last character (the number), to array
-            submissionColumns.push( columnValue.substring( columnValue.length - 1 ) );
-        }
-
-        // get the feedback column
-        const feedbackColumn = tableRows[i].querySelector("td.feedback-column");
-
-        // if the feedback column has a modal
-        if(feedbackColumn.innerText != "N/A") {
-            // dig into the feedback modal and access it's body
-            const feedbackModalBody = feedbackColumn.querySelector(".modal .modal-dialog .modal-content .modal-body");
-            
-            // get both feedback values (feedback stored within <p> under <h6>)
-            const feedbackValue1 = feedbackModalBody.children[1].innerText;
-            const feedbackValue2 = feedbackModalBody.children[3].innerText;
-
-            // add feedback to spreadsheet 
-            // (wrapped in double quotes to indicate to CSV parser to ignore any commas)
-            submissionColumns.push( `"${feedbackValue1}"` );
-            submissionColumns.push( `"${feedbackValue2}"` );
-        }
-        else {
-            submissionColumns.push( "N/A" );
-            submissionColumns.push( "N/A" );
-        }
-
-        // format all columns to one spreadsheet row
-        formattedTableRows.push(submissionColumns.join(","));
+    // run through all rows in current <tbody>
+    for ( const currRow of tableRows ) {
+        // format all columns as one spreadsheet row (columns separated by commas)
+        processedSubmissionRows.push(processRow(currRow, clinicalSiteName));
     }
 
-    // return each spreadsheet row on it's own line
-    console.log(formattedTableRows.join("\n"));
-    return formattedTableRows.join("\n");
 }
 
-function downloadCSV(dataCSV) { 
-    // format the given converted submissions into a csv file
-    const spreadsheetFile = new Blob([dataCSV], { type: "text/csv" }); 
-  
-    // setup url to download newly created .csv file
-    const downloadURL = window.URL.createObjectURL(spreadsheetFile);
-  
-    // grab the export "button" on page
-    const exportToSpreadsheetButton = getByID("export-spreadsheet"); 
-  
-    // set it's href to the download link 
-    exportToSpreadsheetButton.setAttribute("href", downloadURL);
-  
-    // specify that it will be downloading the given file name
-    exportToSpreadsheetButton.setAttribute("download", "nursing-nucleus-survey-submissions.csv"); 
-} 
+function processRow(currRow, clinicalSiteName) {
+    /**
+     * Stores the values of each column in the current <tr>
+     */
+    const submissionColumns = [];
+
+    // add the given clinical site name to row in first column
+    submissionColumns.push(clinicalSiteName);
+
+    // get all rating (number) columns in the current row
+    const ratingColumns = currRow.querySelectorAll("td.rating-column");
+
+    // run through all rating columns in row
+    for ( const currRatingColumn of ratingColumns ) {
+        /**
+         * The \<td\>'s class which stores the value of the rating column.
+         * We are storing the value in the class, because they are displayed as ★'s on the page 
+         */
+        let columnClass = currRatingColumn.classList.item(1);
+
+        // class format is value-#, only display the last character (the number) in column
+        submissionColumns.push( columnClass.substring( columnClass.length - 1 ) );
+    }
+
+    // get the feedback column of current row
+    const feedbackColumn = currRow.querySelector("td.feedback-column");
+
+    // if the feedback column contains a feedback modal
+    if(feedbackColumn.innerText != NOT_APPLICABLE) {
+        // dig into the feedback modal and access it's body
+        const feedbackModalBody = feedbackColumn.querySelector(".modal .modal-dialog .modal-content .modal-body");
+        
+        // get both feedback values (feedback stored within <p> under <h6>)
+        // add feedback to spreadsheet (wrapped in double quotes to 
+        // to CSV parser to ignore any commas)
+        submissionColumns.push( `"${feedbackModalBody.children[1].innerText}"` );
+        submissionColumns.push( `"${feedbackModalBody.children[3].innerText}"` );
+    }
+
+    // if no feedback was given for this row
+    else {
+        submissionColumns.push(NOT_APPLICABLE);
+        submissionColumns.push(NOT_APPLICABLE);
+    }
+
+    return submissionColumns.join(",");
+}
 
 /******************
 ***** HELPERS *****
@@ -140,23 +162,14 @@ function getByID(elementID) {
 }
 
 /**
- * Shortened form of the document.getElementsByClassName method
- * @param {string} className The class name
- * @returns An array of corresponding HTML Elements
+ * Sets up an onclick event for the given HTML element to execute the given function
+ * @param {string} elementID The element's id
+ * @param useFunction The function to be called when the element is clicked
  */
-function getByClass(className) {
-    return document.getElementsByClassName(className);
-}
-
-/**
- * Sets up an onclick event for the given button to execute the given function
- * @param {number} buttonID The button's ID
- * @param useFunction The function to be called when button is clicked
- */
-function setupButtonOnClick(buttonID, useFunction) {
-    // get the button
-    const button = getByID(buttonID);
+function setupOnClick(elementID, useFunction) {
+    // get the element using it's ID
+    const element = getByID(elementID);
 
     // set it's onclick event
-    button.addEventListener("click", useFunction);
+    element.addEventListener("click", useFunction);
 }
--- php/admin/view-entries.php
+++ php/admin/view-entries.php
@@ -146,7 +146,7 @@
                 <?php 
 					// setup "download .csv" button
 					$exportButton = "<a class='col-12 btn btn-success py-2 mt-3 w-100 border' id='export-spreadsheet'>
-										Download Spreadsheet
+										Export to Spreadsheet
 										<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-download' viewBox='0 0 16 16'>
 											<path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5'/>
 											<path d='M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z'/>
@@ -179,6 +179,7 @@
 	<!--Include dynamic scrollspy for mobile-->
 	<script src="/js/responsive-scrollspy-toggle.js"></script>
 
+	<!--Include export to spreadsheet functionality-->
 	<script src="/js/export-to-spreadsheet.js"></script>
 </body>
 </html>
