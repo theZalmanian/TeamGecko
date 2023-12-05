d173ab38a75642f83c623692a2b37a33e3b37cb7
theZalmanian <thezalmanian@gmail.com>
2023-12-05 03:22:59 -0800

    (#37) Done refactoring export-to-spreadsheet.js

--- js/export-to-spreadsheet.js
+++ js/export-to-spreadsheet.js
@@ -1,28 +1,34 @@
-"use strict"; // make JS be more like Java
+"use strict"; // make JS be more strict
 
 // when the page loads
 window.addEventListener("load", function () {  
     // setup export button
-    setupOnClick("export-spreadsheet", downloadCSV("export-spreadsheet"));
+    setupOnClick("export-spreadsheet", setupDownloadCSV("export-spreadsheet"));
 })
 
 /**
- * 
+ * Represents a string containing N/A
  */
 const NOT_APPLICABLE = "N/A";
 
 /**
-* All rows being included in the spreadsheet
+ * The name of the generated .csv file
+ */
+const CSV_FILE_NAME = "experience-survey-submissions";
+
+/**
+* Store processed submission rows for the spreadsheet
 */
-let processedSubmissionRows = []; 
+const processedSubmissionRows = []; 
 
 /**
  * Takes the submission data on view-entries.php, places it within a .csv file,
- * and ties the generated file to the download attribute of the given button
- * @param {string} exportButtonID the ID of the button used to download the generated .csv file
+ * and ties the generated file to the download attribute of button with the given ID
+ * 
+ * @param {string} exportButtonID The ID of the button used to download the generated .csv file
  */
-function downloadCSV(exportButtonID) { 
-    // format the submission data from view-entries appropriately, 
+function setupDownloadCSV(exportButtonID) { 
+    // format the submission data from view-entries currently present within HTML tables, 
     // and then place it within a newly generated .csv file
     const spreadsheetFile = new Blob([formatSubmissionsCSV()], { type: "text/csv" }); 
   
@@ -37,14 +43,14 @@ function downloadCSV(exportButtonID) {
   
     // specify that it will be downloading the .csv file in
     // the href attribute, with the given name
-    exportButton.setAttribute("download", "experience-survey-submissions.csv"); 
+    exportButton.setAttribute("download", CSV_FILE_NAME); 
 } 
 
 /**
  * Formats all experience submissions contained within tables on the view-entries page 
  * into a string that could be processed into a .csv (Comma-Separated Values) spreadsheet
- *
- * @returns {string} all experience submissions contained within tables on the view-entries page 
+ * 
+ * @returns {string} All experience submissions contained within tables on the view-entries page 
  */
 function formatSubmissionsCSV() { 
     /**
@@ -61,6 +67,7 @@ function formatSubmissionsCSV() {
         "Instructor Feedback"
     ];
 
+    // add headers to be first row in spreadsheet
     processedSubmissionRows.push(headerRow.join(","));
 
     // get and run through all clinical site containers on view-entries page
@@ -71,22 +78,22 @@ function formatSubmissionsCSV() {
         // get the current clinical site's name
         const clinicalSiteName = submissionContainer.querySelector("h1").innerText;
 
-        processTable(submissionContainer.querySelector("table tbody"), clinicalSiteName);
+        // process the current table
+        processTableRows(submissionContainer.querySelector("table tbody"), clinicalSiteName);
     }
   
-    // return all clinical site rows within a string, 
-    // each string of rows per clinical site separated by a newline
-    console.log(processedSubmissionRows);
-    return processedSubmissionRows.join("\n"); 
+    // once all submissions on page have been formatted and placed within array,
+    // return all clinical site rows within a string, separating each row by a newline marker
+    return processedSubmissionRows.join("\n");
 } 
 
 /**
- * 
- * @param {*} currTbody 
- * @param {*} clinicalSiteName 
- * @returns 
+ * Formats the submission rows within the given \<tbody\> and adds them to the processedSubmissionRows array
+ *
+ * @param {HTMLElement} tableBody The \<tbody\> corresponding to the clinical site currently being processed
+ * @param {string} clinicalSiteName The name of the clinical site currently being processed
  */
-function processTable(currTbody, clinicalSiteName) {
+function processTableRows(currTbody, clinicalSiteName) {
     /**
      * All \<tr\>'s in the given \<tbody\>
      */
@@ -94,57 +101,60 @@ function processTable(currTbody, clinicalSiteName) {
 
     // run through all rows in current <tbody>
     for ( const currRow of tableRows ) {
-        // format all columns as one spreadsheet row (columns separated by commas)
-        processedSubmissionRows.push(processRow(currRow, clinicalSiteName));
+        // process the current <tr>, and add the formatted submission to the array
+        processedSubmissionRows.push(processSubmissionRow(currRow, clinicalSiteName));
     }
-
 }
 
-function processRow(currRow, clinicalSiteName) {
+/**
+ * Processes the given \<tr\> and returns a string representing the submission row, 
+ * formatted for a .csv spreadsheet
+ *
+ * @param {HTMLElement} currRow The \<tr\> element being processed
+ * @param {string} clinicalSiteName The name of the clinical site the submission row belongs to
+ * @returns {string} A string representing the submission row, formatted for a .csv spreadsheet
+ */
+function processSubmissionRow(currRow, clinicalSiteName) {
     /**
-     * Stores the values of each column in the current <tr>
+     * Stores the values of each column in the current \<tr\>
      */
     const submissionColumns = [];
 
     // add the given clinical site name to row in first column
     submissionColumns.push(clinicalSiteName);
 
-    // get all rating (number) columns in the current row
-    const ratingColumns = currRow.querySelectorAll("td.rating-column");
-
-    // run through all rating columns in row
-    for ( const currRatingColumn of ratingColumns ) {
+    // get and run through all rating (number) columns in the current row
+    for ( const currRatingColumn of currRow.querySelectorAll("td.rating-column") ) {
         /**
-         * The \<td\>'s class which stores the value of the rating column.
-         * We are storing the value in the class, because they are displayed as ★'s on the page 
+         * The \<td\>'s class which stores the value of the rating column. We are storing 
+         * the value in the class, because they are displayed as ★'s on the page 
          */
-        let columnClass = currRatingColumn.classList.item(1);
+        const ratingValueClass = currRatingColumn.classList.item(1);
 
-        // class format is value-#, only display the last character (the number) in column
-        submissionColumns.push( columnClass.substring( columnClass.length - 1 ) );
+        // class format is value-#, so extract the number and display in column
+        submissionColumns.push( ratingValueClass.substring( ratingValueClass.length - 1 ) );
     }
 
     // get the feedback column of current row
     const feedbackColumn = currRow.querySelector("td.feedback-column");
 
     // if the feedback column contains a feedback modal
-    if(feedbackColumn.innerText != NOT_APPLICABLE) {
+    if(feedbackColumn.innerText.trim() != NOT_APPLICABLE) {
         // dig into the feedback modal and access it's body
         const feedbackModalBody = feedbackColumn.querySelector(".modal .modal-dialog .modal-content .modal-body");
         
         // get both feedback values (feedback stored within <p> under <h6>)
-        // add feedback to spreadsheet (wrapped in double quotes to 
-        // to CSV parser to ignore any commas)
+        // and add to spreadsheet (using `"$"` to indicate to .csv parser to ignore any commas within)
         submissionColumns.push( `"${feedbackModalBody.children[1].innerText}"` );
         submissionColumns.push( `"${feedbackModalBody.children[3].innerText}"` );
     }
 
     // if no feedback was given for this row
     else {
-        submissionColumns.push(NOT_APPLICABLE);
-        submissionColumns.push(NOT_APPLICABLE);
+        submissionColumns.push(NOT_APPLICABLE, NOT_APPLICABLE);
     }
 
+    // return a string containing all submission columns separated by commas
     return submissionColumns.join(",");
 }
 
